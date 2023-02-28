8009293a971a110a9a9ea7a50f4bdbe8be74358f
Kacper Kinius <69329456+kiniusiekkk@users.noreply.github.com>
2023-02-28 17:30:29 +0100

    Update clickButton.js

--- Poradnik #8 - Tickety/events/clickButton.js
+++ Poradnik #8 - Tickety/events/clickButton.js
@@ -1,82 +1,71 @@
-const { MessageEmbed } = require('discord.js')
-const { MessageButton, ButtonCollector } = require('discord-buttons')
-
-
+const { MessageEmbed, Integration } = require('discord.js')
+const { MessageButton, MessageActionRow } = require('discord-buttons')
+const interakcje = require("../interakcje.json");
 module.exports = async (client, button) => {
     await button.reply.defer()
     let buttonClicker = button.clicker.member;
     let guild = button.guild;
 
-
-    if(button.id === "PRZYCISK_WERYFIKACJA") {
-        let EmbedSukcesu = new MessageEmbed()
-        .setAuthor(`Twój Serwer - Weryfikacja`, "https://devilhub.cf/logo.png/")
-        .setColor('GREEN')
-        .setDescription('Pomyślnie się ZWERYFIKOWAŁEŚ/AŚ')
-        let EmbedProblem = new MessageEmbed()
-        .setAuthor(`Twój Serwer - Weryfikacja`, "https://devilhub.cf/logo.png/")
-        .setColor('GREEN')
-        .setDescription('Pomyślnie się ZWERYFIKOWAŁEŚ/AŚ')
-
-
-        if(!buttonClicker.roles.cache.has('1076913756237463572')) {
-            buttonClicker.roles.add('1076913756237463572')
-            buttonClicker.send(EmbedSukcesu)
-        } else {
-            buttonClicker.send(EmbedProblem)
+    if (button.id === 'TICKET_KINIUS_OPEN') {
+        let alreadyOpenedTicket = client.channels.cache.filter(m => m.type == "text" && m.name.includes("ticket-")).map(m => m.name.split("ticket-")[1]);
+        let współpraca = alreadyOpenedTicket.some(v => buttonClicker.user.username == v)
+        console.log(współpraca)
+        if (współpraca === true) {
+            return await buttonClicker.user.send(new MessageEmbed().setAuthor("Coś poszło nie tak!", "https://cdn.discordapp.com/emojis/751937063649017927.gif?v=1").setColor('#6600ff').setDescription(`\`Posiadasz już utworzony ticket!\``))
         }
-    }
-
-
-
-    if(button.id === "TICKET_KINIUS_OPEN") {
-        let alreadyOpenedTicket = client.channels.cache.filter(m => m.type == "text" &&  m.name.includes("ticket-")).map(m => m.name.split("ticket-")[1])
-        let bilecik = alreadyOpenedTicket.some(v => buttonClicker.user.username == v)
-        console.log(bilecik)
-
-
-        if(bilecik === true ) {
-            return await buttonClicker.user.send(new MessageEmbed().setAuthor('Wystąpił bląd', "https://devilhub.cf/logo.png").setColor('BLACK').setDescription("Posiadasz już ticket"))
-        }
-
         let ticketChannel = await guild.channels.create(`Ticket-${buttonClicker.user.username}`, {
             type: "text",
+
+
+            //id kategorii
+            parent: "1014203581978644591",
+
             permissionOverwrites: [
                 {
-                id: buttonClicker.user.id,
-                allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "ATTACH_FILES"]
-                },
-                { id: guild.roles.everyone,
-                  deny: ["SEND_MESSAGES", "VIEW_CHANNEL", "ATTACH_FILES"]
+                    id: buttonClicker.user.id,
+                    allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "ATTACH_FILES"]
                 },
                 {
-                    id: `1076917888742608926`,
+                    id: guild.roles.everyone,
+                    deny: ["SEND_MESSAGES", "VIEW_CHANNEL", "ATTACH_FILES"]
+                },
+                {
+                    //id roli która ma dostep do widzenia kanalu
+                    id: '1001590984003555396', 
+
+
                     allow: ["SEND_MESSAGES", "VIEW_CHANNEL", "ATTACH_FILES"]
                 }
             ]
         })
         let openedTicket = new MessageEmbed()
-        .setAuthor('TICKETY', "https://devilhub.cf/logo.png")
-        .setColor('YELLOW')
-        .setDescription('Czekaj, aż ktoś ci odpisze...')
-
-        let ZamknijButton = new MessageButton()
-        .setLabel('Zamknij Ticket')
-        .setStyle("red")
-        .setID("TICKET_KINIUS_CLOSE")
-        ticketChannel.send(buttonClicker.user, {
+            .setAuthor(`DevilHub`, "https://devilhub.cf/logo.png")
+             .setColor('RED')
+            
+            .setDescription("> Cześć ***" + buttonClicker.user.username + "*** w czym możemy ci pomóc?\n> Napisz tutaj z czym masz problem a napewno zaraz ktoś się tobą zajmię!")
+        let supportButton = new MessageButton()
+            .setLabel("Zamknij ticket")
+            .setEmoji("❌")
+            .setStyle("blurple")
+            .setID("TICKET_KINIUS_CLOSE")
+        ticketChannel.send(`${buttonClicker.user}`, {
             embed: openedTicket,
-            buttons: [ZamknijButton]
+            components: new MessageActionRow()
+                .addComponent(supportButton)
         })
+
+
+              console.log(`Utworzono Ticket-${buttonClicker.user.username} przez ${buttonClicker.user.id}`)
+    }
     if (button.id === `TICKET_KINIUS_CLOSE`) {
         let ticketChannel = button.channel;
 
         let deletingTicket = new MessageEmbed()
-            .setAuthor(`DevilHub`, "https://devilhub.cf/logo.png")
-                   .setColor('RED')
+             .setAuthor(`DevilHub`, "https://devilhub.cf/logo.png")
+             .setColor('RED')
             .setDescription(`Zostanie on usunięty za **5** sekund!`)
         ticketChannel.send(deletingTicket)
         setTimeout(() => { ticketChannel.delete() }, 5000);
     }
-    }
+
 }
